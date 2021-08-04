4e890e3e987b9f4ba70a8065037da4219f889778
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-08-03 23:07:01 -0300

    :memo: Add Open API specification

--- src/tasks/task.dto.ts
+++ src/tasks/task.dto.ts
@@ -1,19 +1,25 @@
 import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTaskDto {
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   readonly id: number;
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly title: string;
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly description: string;
+  @ApiProperty()
   @IsBoolean()
   @IsNotEmpty()
   readonly completed: boolean;
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   readonly favorites: number;
